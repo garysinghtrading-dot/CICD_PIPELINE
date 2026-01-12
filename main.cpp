66680799d0943218fa0df2fa599ec9ed787db05f
garysinghtrading-dot <gary.singh.trading@gmail.com>
2026-01-12 14:22:10 -0800

    Update main.cpp
    Added tests for add method

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
+#include <cassert>
 
-int main(){
-  cout << "Hello world" << endl;
-  return 0;
+// Include your function
+int add(int a, int b){  // forward declaration
+  return a+b;
+}
+
+
+void test_add() {
+    assert(add(1, 2) == 3);
+    assert(add(-5, 5) == 0);
+    assert(add(10, -3) == 7);
+}
+
+int main() {
+    test_add();
+    std::cout << "All tests passed!\n";
+    return 0;
 }
